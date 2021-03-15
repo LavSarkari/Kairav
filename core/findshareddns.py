061f0f26ce34494a441e0a4f9e4467db3f5d641f
LavSarkari <LavSarkari@pm.me>
2021-03-15 13:28:47 +0530

    Create findshareddns.py

---
+++ core/findshareddns.py
@@ -0,0 +1,29 @@
+
+import os,sys
+import time
+import requests
+
+
+u="\033[4m"
+w="\033[00m"
+r="\033[91;1m"
+b="\033[34;1m"
+y="\033[33;1m"
+g="\033[32;1m"
+
+def start():
+ try:
+        print ("")
+        print (g+"\n [+] DNS Scanner ! ! !")
+        website = str(input(b+" [+]"+w+" Domain/IP target: "))
+        result = requests.get('https://api.hackertarget.com/findshareddns/?q=' + website)
+        print (y+result)
+        print ("")
+        text = str(input(w+"[ enter ]"))
+        if text == " ":
+                os.system(Kairav)
+        else:
+                sys.exit(0)
+ except:
+        input('[ ENTER ]')
+        os.system('Kairav')
